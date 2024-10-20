9400281d4981793cafbf86728efc5068eb4aa84d
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-10-20 10:57:48 +0530

    Push1

--- Uprecyclemarket/src/components/NavigatioN.jsx
+++ Uprecyclemarket/src/components/NavigatioN.jsx
@@ -13,13 +13,16 @@ export default function Navigation() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           RecycleMarketplace
         </Typography>
-        <Button color="inherit" component={RouterLink} to="/">
+        <Button color="inherit" component={RouterLink} to="/" sx={{ marginRight: 2 }}>
+          <img src="Uprecycle-Market/Uprecyclemarket/src/assets/home.png" alt="Logo" style={{ width: 24, height: 24}} />
           Home
         </Button>
-        <Button color="inherit" component={RouterLink} to="/marketplace">
+        <Button color="inherit" component={RouterLink} to="/marketplace" sx={{ marginRight: 2 }}>
+          <img src="Uprecycle-Market/Uprecyclemarket/src/assets/market.png" alt="Market" style={{ width: 24, height: 24}} />
           Marketplace
         </Button>
         <Button color="inherit" component={RouterLink} to="/scrap-collection">
+          <img src="Uprecycle-Market/Uprecyclemarket/src/ssets/recycle.png" alt="Sell" style={{ width: 24, height: 24}} />
           Sell Scrap
         </Button>
       </Toolbar>
